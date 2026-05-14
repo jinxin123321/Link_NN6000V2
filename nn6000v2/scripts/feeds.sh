40641390d51d0c1b1977f8454d3a952be24db529
jinxin123321 <122540131+jinxin123321@users.noreply.github.com>
2026-05-14 22:42:46 +0800

    Update feeds.sh

--- nn6000v2/scripts/feeds.sh
+++ nn6000v2/scripts/feeds.sh
@@ -10,7 +10,7 @@ update_feeds() {
 
     if ! grep -q "openwrt-packages" "$FEEDS_PATH"; then
         [ -z "$(tail -c 1 "$FEEDS_PATH")" ] || echo "" >>"$FEEDS_PATH"
-        echo "src-git openwrt_packages https://github.com/kenzok8/openwrt-packages.git" >>"$FEEDS_PATH"
+        echo "src-git openwrt_packages https://github.com/jinxin123321/openwrt-packages.git" >>"$FEEDS_PATH"
     fi
 
     if [ ! -f "$BUILD_DIR/include/bpf.mk" ]; then
